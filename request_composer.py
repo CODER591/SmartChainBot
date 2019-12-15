3d93afe9cdb06584a6feb01ab260f025f90b7ce4
CODER591 <mazorchyk@gmail.com>
2019-12-15 16:21:57 +0200

    Fixed indent issue in parameters.py

--- request_composer.py
+++ request_composer.py
@@ -6,16 +6,19 @@
 #composing depending on received value
 
 #parameters are very much dynamic value.
-
+from parameters import *
 from request import *
-
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 key = ''
-parameters = {
+'''parameters = {
   'start':'1',
   'limit':'5000',
   'convert':'USD'
-}
+}'''
+headers = {
+      'Accepts': 'application/json',
+      'Accept-Encoding': 'deflate, gzip',
+   }
 # Type: class
 # Name: RequestComposer
 #
@@ -23,13 +26,13 @@ parameters = {
 #
 # Functional: 
 #   approppriate headers and parameters setting, making request by using RequestProc class
-class RequestComposer:
-    headers = {
-      'Accepts': 'application/json',
-      'Accept-Encoding': 'deflate, gzip',
-    }
+#class RequestComposer:
+#    headers = {
+#      'Accepts': 'application/json',
+#      'Accept-Encoding': 'deflate, gzip',
+#    }
 
-    def __init__(self):
+#    def __init__(self):
 
 
 
@@ -37,8 +40,8 @@ class RequestComposer:
 
 
 #move it to some command or wrapper, in any case remove this strings from this file
-#new_req=RequestProc(headers,key);
-#new_req.execRequest(parameters,url);
+new_req=RequestProc(headers,key);
+new_req.execRequest(Crypto_Map('active',1,5000,'name',symbol='',aux='platform'),url);
 #new_req.execRequest(parameters,url);
 
 
